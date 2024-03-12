adf1d548e08b2813f79d9011fd0b1823d4299627
shaoshixuan <161456285+shaoshixuan@users.noreply.github.com>
2024-03-12 22:44:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.write("hello world")
+function sayHello(){
+  var response = prompt("what is u name?");
+  alert("hello  " + response + ",have a nice day!");
+};
+//sayHello();
+function showTopic(){
+  var x=document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color="red";
+};
+showTopic();
